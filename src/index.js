b47d94cc29f78522e59eab80398b8e4fdceb7c3f
saa3 <saa33-saa33@yandex.ru>
2017-07-04 13:08:31 +0300

    Redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,86 @@
-import React from 'react';
+import React, {PropTypes, } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+/*import { connect } from 'react-redux';*/
+
+const GET_ITEMS = "GET_ITEMS";
+
+const getItems = () => {
+  return { type : GET_ITEMS}
+}
+
+const initialState = {
+    items: []
+}
+
+const todoApp = (state = initialState, action) => {
+  const catalog = [
+    {imgsrc: require("../data/img/1.jpg"), name: "bick", price: 250},
+    {imgsrc: require("../data/img/2.jpg"), name: "rocket", price: 200},
+    {imgsrc: require("../data/img/3.jpg"), name: "pokemon", price: 300}
+  ]
+  switch (action.type) {
+    case GET_ITEMS:
+      return Object.assign( {}, state, {
+        items : catalog
+      });
+    default:
+      return state;
+  }
+}
+
+let store = createStore(todoApp);
+
+store.dispatch(getItems());
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items,
+  }
+}
+
+const Item = ({imgsrc, name, price}) => (
+  <div className="item">
+    <mapStateToProps />
+    <img src={imgsrc} alt={name}/>
+    <div className="name">
+      {name}
+    </div>
+    <div className="price">
+      {price}
+    </div>
+  </div>
+)
+
+Item.PropTypes = {
+  imgsrc : PropTypes.object.isRequired,
+  name : PropTypes.string.isRequired,
+  price : PropTypes.number.isRequired
+}
+
+const ItemList = ({items, }) => (
+  <div className="items">
+    {items.map(item =>
+      <Item
+        imgsrc={item.imgsrc}
+        name={item.name}
+        price={item.price}
+      />
+    )}
+  </div>
+)
+
+ItemList.PropTypes = {
+  items : PropTypes.arrayOf(PropTypes.shape({
+    imgsrc: PropTypes.object.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
+  }).isRequired).isRequired,
+}
+
+const VisibleItemList = connect( mapStateToProps )(ItemList);
 
 const Head = () => (
   <div className="head">
@@ -11,37 +91,11 @@ const Head = () => (
   </div>
 );
 
-class Body extends React.Component {
-  constructor() {
-    super();
-    this.catalog = [
-      {imgsrc: require("../data/img/1.jpg"), name: "bick", price: 250},
-      {imgsrc: require("../data/img/2.jpg"), name: "rocket", price: 200},
-      {imgsrc: require("../data/img/3.jpg"), name: "pokemon", price: 300}
-    ]
-  }
-  render () {
-    const items = this.catalog.map((item) => {
-      return (
-        <div className="item">
-          <img src={item.imgsrc} alt={item.name}/>
-          <div className="name">
-            {item.name}
-          </div>
-          <div className="price">
-            {item.price}
-          </div>
-        </div>
-      );
-    });
-
-    return (
-      <div className="items">
-        {items}
-      </div>
-    );
-  }
-}
+const Body = () => (
+  <div className="body">
+    <VisibleItemList />
+  </div>
+);
 
 const Footer = () => (
   <div className="footer">
@@ -58,6 +112,8 @@ const Shop = () => (
 );
 
 ReactDOM.render(
-  <Shop />,
+  <Provider store={store}>
+    <Shop />
+  </Provider>,
   document.getElementById("root")
 );
