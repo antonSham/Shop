c0de93668fb4d6d015b8b815e0ae806140e8bde9
saa3 <saa33-saa33@yandex.ru>
2017-07-18 12:29:25 +0300

    Header

---
+++ src/components/CartBadge.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export default styled.span`
+  position: absolute;
+  left: 25px;
+  top: 30px;
+  background-color: red;
+`;
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,15 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Title from "./Title.js";
-import StyledHead from "./StyledHead.js";
+import { connect } from "react-redux";
+import CartLogo from "./CartLogo.js";
+import CartLogoImg from "./CartLogoImg.js";
+import CartBadge from "./CartBadge.js";
 
-export default () =>
-  <StyledHead>
-    <Link to="/">
-      <img src={require("../../data/img/logo.jpg")} alt="logo" />
-    </Link>
-    <Title>The best toe shop</Title>
-    <Link to="/Cart">
-      <img src={require("../../data/img/cart.jpg")} alt="cart" />
-    </Link>
-  </StyledHead>;
+const mapStateToProps = (state, ownProps) => ({
+  cartItemsCount: state.cart.length
+});
+
+const Head = ( {cartItemsCount} ) =>
+  <div className="uk-navbar-container" data-uk-navbar>
+    <div className="uk-navbar-left">
+      <Link to="/" className="uk-navbar-item uk-logo">
+        ToeShop
+      </Link>
+    </div>
+    <div className="uk-navbar-right">
+      <Link to="/Cart" className="uk-navbar-item uk-logo">
+        <CartLogo>
+          <CartLogoImg src={require("../../data/img/cart.gif")} alt="cart" />
+          <CartBadge
+            className={"uk-badge " + (cartItemsCount!==0 ? "" : "uk-invisible")}>
+              {cartItemsCount}
+          </CartBadge>
+        </CartLogo>
+      </Link>
+    </div>
+  </div>;
+
+export default connect(mapStateToProps)(Head);
